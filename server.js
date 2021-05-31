eb2b74f9693f07d9094c0ba6ca854ec3c260592f
Zoheb Alli Khan <understandingzak@gmail.com>
2021-05-31 15:28:04 +0530

    serveradded

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ app.all("/", (req, res) => {
   res.send("Bot is running");
 });
 
+function live() {
+  app.listen("https://incubusdisbot.herokuapp.com/", () => {
+    console.log("The bot is up in the server");
+  });
+}
+
 module.exports = live;
