2576e8362bfccfd43e58736f204004e19ff869eb
Zoheb Alli Khan <understandingzak@gmail.com>
2021-05-31 15:17:15 +0530

    Hey there

--- server.js
+++ server.js
@@ -6,10 +6,4 @@ app.all("/", (req, res) => {
   res.send("Bot is running");
 });
 
-function live() {
-  app.listen(3000, () => {
-    console.log("server is running");
-  });
-}
-
 module.exports = live;
